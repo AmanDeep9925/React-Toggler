daafda1785abe9f70efeb1ba2dfa2a26f40bce92
AmanDeep9925 <amandeep58257@gmail.com>
2019-07-06 18:33:15 +0530

    Toggler Component

--- src/components/ToggleSwitch/index.js
+++ src/components/ToggleSwitch/index.js
@@ -1,81 +1,80 @@
-/_ src/components/ToggleSwitch/index.js 
-
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import isString from 'lodash/isString';
-import React, {Components} from 'react';
+import React, { Component } from 'react';
 import isBoolean from 'lodash/isBoolean';
 import isFunction from 'lodash/isFunction';
+import './index.css';
 
-import "./index.css";
+class ToggleSwitch extends Component {
 
-class ToggleSwitch extends Components{
-    state = {enabled:this.enabledFromProps()}
+  state = { enabled: this.enabledFromProps() }
 
-    isEnabled = () => this.state.enabled
+  isEnabled = () => this.state.enabled
+
+  enabledFromProps() {
+    let { enabled } = this.props;
+
+    // If enabled is a function, invoke the function
+    enabled = isFunction(enabled) ? enabled() : enabled;
+
+    // Return enabled if it is a boolean, otherwise false
+    return isBoolean(enabled) && enabled;
+  }
+
+  toggleSwitch = evt => {
+    evt.persist();
+    evt.preventDefault();
+
+    const { onClick, onStateChanged } = this.props;
+
+    this.setState({ enabled: !this.state.enabled }, () => {
+      const state = this.state;
+
+      // Augument the event object with SWITCH_STATE
+      const switchEvent = Object.assign(evt, { SWITCH_STATE: state });
+
+      // Execute the callback functions
+      isFunction(onClick) && onClick(switchEvent);
+      isFunction(onStateChanged) && onStateChanged(state);
+    });
+  }
+
+  render() {
+    const { enabled } = this.state;
     
-    // This Method resolves the enabled prop that was passed as a boolean  
-    enabledFromProps(){
-        let{enabled} = this.props;
+    // Isolate special props and store the remaining as restProps
+    const { enabled: _enabled, theme, onClick, className, onStateChanged, ...restProps } = this.props;
 
-        enabled = isFunction(enabled) ? enabled() : enabled;
+    // Use default as a fallback theme if valid theme is not passed
+    const switchTheme = (theme && isString(theme)) ? theme : 'default';
 
-        return isBoolean(enabled) && enabled;
-    }
+    const switchClasses = classnames(
+      `switch switch--${switchTheme}`,
+      className
+    )
 
-    toggleSwitch = evt => {
-        evt.persist();
-        evt.perventDefault();
+    const togglerClasses = classnames(
+      'switch-toggle',
+      `switch-toggle--${enabled ? 'on' : 'off'}`
+    )
 
-        const {onClick, onStateChanged} = this.props;
+    return (
+      <div className={switchClasses} onClick={this.toggleSwitch} {...restProps}>
+        <div className={togglerClasses}></div>
+      </div>
+    )
+  }
 
-        this.setState({enabled: !this.state.enabled},() => {
-            const state = this.state;
-
-            const switchEvent = Object.assign(evt, {SWITCH_STATE: state});
-
-            isFunction(onClick) && onClick(switchEvent);
-            isFunction(onStateChanged) && onStateChanged(state);
-        });
-    }
-
-    render(){
-        const { enabled } = this.state;
-
-        // Isolate special props and store the remaining as restProps
-
-        const { enabled: _enabled,theme,onClick,className, onStateChanged, ...restPorps } = this.props;
-
-        const switchTheme = (theme && isString(theme)) ? theme : 'default';
-
-        const switchClasses = classnames(
-            `switch switch--${switchTheme}`,
-            className
-        )
-
-        const toggleClasses = classnames(
-            'switch-toogle',
-            `switch-toggle--${enabled? 'on' : 'off'}`
-        )
-
-        return(
-            <div className = {switchClasses} onClick = {this.toggleSwitch} {...restPorps}>
-                <div className = {toggleClasses}></div>
-            </div>
-        )
-    }
 }
 
 ToggleSwitch.propTypes = {
-    // Indicating the style and color to be used for the Toggle Switch
-    theme:PropTypes.string,
-    // Can be either a boolean or a function that returns a boolean
-    enabled: ProppTypes.oneOfType([
-        ProppTypes.bool,
-        ProppTypes.func
-    ]),
-    // CallBack Function changes the state of the toggle
-    onStateChanged: ProppTypes.func
+  theme: PropTypes.string,
+  enabled: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.func
+  ]),
+  onStateChanged: PropTypes.func
 }
 
 export default ToggleSwitch;
